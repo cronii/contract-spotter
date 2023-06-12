124094e62f7b2e1180e0d06db034983f6f5eccc3
cronii <cronii@protonmail.com>
2023-06-11 23:53:33 -0400

    record processing timestamp + better logging

--- get-contract-names.js
+++ get-contract-names.js
@@ -16,10 +16,18 @@ const ERROR_LOG = './output/errorlog-get-contract-name';
       driver: sqlite3.Database
     });
 
-    const contractAddresses = await db.all('SELECT * FROM v_contract_info WHERE name IS NULL');
+    const timestamp = Date.now();
+    let updates = 0;
+
+    const query = `SELECT contracts.address FROM contracts
+    LEFT JOIN contract_name
+    ON contracts.address = contract_name.address
+    WHERE contract_name.name IS NULL
+    AND contract_name.processed IS NULL`;
+    const contractAddresses = await db.all(query);
     console.log(`found ${contractAddresses.length} contracts`);
 
-    await db.run('CREATE TABLE IF NOT EXISTS contract_name (address TEXT PRIMARY KEY, name TEXT)');
+    await db.run('CREATE TABLE IF NOT EXISTS contract_name (address TEXT PRIMARY KEY, name TEXT, processed INT)');
 
     for (const contractAddress of contractAddresses) {
       const { address } = contractAddress;
@@ -29,17 +37,20 @@ const ERROR_LOG = './output/errorlog-get-contract-name';
         const contract = new ethers.Contract(address, ABI, provider);
         const name = await contract.name();
 
-        await db.run('INSERT OR REPLACE INTO contract_name (address, name) VALUES (?, ?)', [address, name]);
+        await db.run('INSERT OR REPLACE INTO contract_name (address, name, processed) VALUES (?, ?, ?)', [address, name, timestamp]);
 
         console.log(`${address}: ${name}`);
+        updates++;
       } catch (error) {
-        await fs.promises.appendFile(ERROR_LOG, JSON.stringify(error, null, 2));
+        await db.run('INSERT OR REPLACE INTO contract_name (address, name, processed) VALUES (?, ?, ?)', [address, null, timestamp]);
+        await fs.promises.appendFile(`${ERROR_LOG}-${timestamp}`, JSON.stringify(error, null, 2));
       }
 
       console.timeEnd(address);
     }
 
-    console.log(`processed ${contractAddresses.length} contracts`);
+    console.log(`${contractAddresses.length} contracts processed`);
+    console.log(`${updates} updated contracts`);
     await db.close();
   } catch (err) {
     console.error(err);
